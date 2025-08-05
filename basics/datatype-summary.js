a04b11f29224a40ca2709d7e5f5fab5c7c7ad83b
zansipatel511-source <zansipatel511@gmail.com>
2025-08-05 11:14:51 +0000

    memory done

--- basics/datatype-summary.js
+++ basics/datatype-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);//
 
 // const bigNumber = 751236974123658n//
 
@@ -32,9 +32,34 @@ const myFunction = function(){
     console.log("Hello World")
 }
 
-console.log(typeof bigNumber);
+/*console.log(typeof bigNumber);
 console.log(typeof outsideTemp);
 console.log(typeof scoreValue);
 console.log(typeof myFunction);
 console.log(typeof heros);
-console.log(typeof anotherId);
+console.log(typeof anotherId);*/
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap(Non-Primitive)
+
+let myName = "Zansi"
+
+let anothername = myName
+anothername = "Janu"
+
+console.log(myName);
+console.log(anothername);
+
+let userOne = {
+    email: "zansi@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "janu511@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
