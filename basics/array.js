22c5e68c2961f136ccabe128e55fc44226de5353
zansipatel511-source <zansipatel511@gmail.com>
2025-08-06 11:05:47 +0000

    array done

--- basics/array.js
+++ basics/array.js
@@ -12,23 +12,34 @@ console.log(name5);*/
 /*var arr = ["Zansi", "Kavya", "Dharm", "Parv", "Divya"];
 console.log(arr);*/
 
+
+// Code 2: Declare and Print 3 students' names using an array //
 /*var names = ["Zansi", "Kavya", "Dharm"];
 console.log(names[0]);
 console.log(names[1]);
 console.log(names[2]);*/
 
 
+
+// Code 3: Create an array of vegetables, store 3 vegetables, and print them //
 /*var vegetables = ["Tomato", "Beans", "Onion"];
 console.log(vegetables[0]);
 console.log(vegetables[1]);
 console.log(vegetables[2]);*/
 
 
+
+// Code 4: Find the length of the vegetables array //
 /*var vegetables = ["Tomato", "Beans", "Onion"];
 console.log(vegetables.length);*/
 
+
+
+// Code 5: Create an array price, store prices, and print the last price //
 /*var price = [50, 100, 5000];
 console.log(price[2]);*/
+
+
 
 
 /*var prices = [45, 71, 29, 5000];
@@ -36,6 +47,8 @@ var last_index = prices.length - 1;
 console.log(prices[last_index]);*/
 
 
+
+// Code 6: Insert 3 movie names in the array using push() //
 /*var items = [];
 items.push("Bahuballi");
 items.push("Avengers");
@@ -43,6 +56,8 @@ items.push("Spider Man");
 console.log(items);*/
 
 
+
+// Code 7: Create an array superheroes, push 3 superheroes, and print the array//
 /*var superheroes = [];
 superheroes.push("batman");
 superheroes.push("superman");
@@ -51,6 +66,7 @@ console.log(superheroes);*/
 
 
 
+// Code 8: Print all elements of the array using a loop //
 /*var movies = ["batman", "superman", "ironman"];
 for (var i = 0; i < movies.length; i++) {
     console.log(movies[i]);
@@ -58,6 +74,8 @@ for (var i = 0; i < movies.length; i++) {
 
 
 
+
+// Code 9: Create an array of movies and actors, print all movie names with actors //
 /*var movies = ["Bahuballi", "Spider Man", "Iron Man", "Super Man"];
 var actors = ["Prabhas", "Tom Holland", "Robert Downey", "Henry Cavill"];
 for (var i = 0; i < movies.length; i++) 
@@ -66,6 +84,8 @@ for (var i = 0; i < movies.length; i++)
     }*/
 
 
+
+// Code 10: Pop the last 2 elements from an array //
 /*var movies = ["batman", "superman", "ironman"];
 movies.pop();
 movies.pop();
@@ -73,6 +93,8 @@ console.log(movies);*/
 
 
 
+
+// Code 11: Create an array of 6 numbers, print the numbers array, delete last 3 numbers, and print the array //
 /*var numbers = [2, 3, 4, 5, 6, 7];
 console.log(numbers);
 numbers.pop();
@@ -81,7 +103,7 @@ numbers.pop();
 console.log(numbers);*/
 
 
-
+// Second Way: //
 /*var numbers = [2, 3, 4, 5, 6, 7];
 console.log(numbers);
 for (var i = 1; i <= 3; i++) {
@@ -90,6 +112,9 @@ for (var i = 1; i <= 3; i++) {
 console.log(numbers);*/
 
 
+
+
+// Code 12: Print the first 3 items in the array using a loop //
 /*var movies = ["bahuballi", "Spider Man", "Iron Man", "Super Man"];
 for (var i = 1; i <= 3; i++)
     {
@@ -97,6 +122,7 @@ for (var i = 1; i <= 3; i++)
 }*/
 
 
+// Second Way [ Using Break ]: //
 /*var movies = ["bahuballi", "Spider Man", "Iron Man", "Super Man"];
 for (var i = 0; i < movies.length; i++) {
     if (i == 3) {
@@ -106,6 +132,8 @@ for (var i = 0; i < movies.length; i++) {
 }*/
 
 
+
+// Code 13: Print all movies except the third movie //
 /*var movies = ["bahuballi", "Spider Man", "Iron Man", "Super Man"];
 for (var i = 0; i < movies.length; i++) {
     if (i == 2) {
@@ -116,6 +144,8 @@ for (var i = 0; i < movies.length; i++) {
 
 
 
+
+// Code 15: For Even Array, print the second half of the array //
 /*var movies = ["bahuballi", "Spider Man", "Iron Man", "Super Man", "hulk", "thor"];
 for (var i = 0; i < movies.length; i++) {
     if (i == 2 || i == 4) {
@@ -125,6 +155,8 @@ for (var i = 0; i < movies.length; i++) {
 }*/
 
 
+
+// Code 16: For Even or Odd Array, print the second half of the array //
 /*var names = ["A", "B", "C", "D", "E", "F", "G", "H"];
 var start = Math.floor(names.length / 2);
 for (var i = start; i < names.length; i++) {
@@ -134,6 +166,8 @@ for (var i = start; i < names.length; i++) {
 
 
 
+
+// Code 17: For Even or Odd Array, print the first half of the array //
 /*var names = ["A", "B", "C", "D", "E", "F", "G", "H", "K", "L"];
 var start = 0;
 if (names.length % 2 == 0) {
@@ -148,6 +182,8 @@ for (var i = start; i < names.length; i++) {
 
 
 
+
+// Code 18: Given marks, find the total marks* //
 /*var marks = [10, 15, 19, 20, 21];
 var sum = 0;
 for (var i = 0; i < marks.length; i++) {
@@ -157,6 +193,8 @@ console.log(sum);*/
 
 
 
+
+// Code 19: Find the sum of all subject marks and average also //
 /*var subject_marks = [10, 15, 19, 20, 21];
 var sum_marks = 0;
 for (var i = 0; i < subject_marks.length; i++) {
@@ -169,6 +207,7 @@ console.log("Average is ", average);*/
 
 
 
+// Code 20: Given marks, find the maximum marks //
 /*var marks = [10, 15, 19, 20, 21, 45, 31, 18];
 var max = -Infinity;
 for (var i = 0; i < marks.length; i++) {
