07dac12c6c9101f111ec28a9b9d72000b88aacfc
zansipatel511-source <zansipatel511@gmail.com>
2025-08-06 11:12:08 +0000

    array done

--- basics/array.js
+++ basics/array.js
@@ -116,7 +116,7 @@ console.log(numbers);*/
 
 // Code 12: Print the first 3 items in the array using a loop //
 /*var movies = ["bahuballi", "Spider Man", "Iron Man", "Super Man"];
-for (var i = 1; i <= 3; i++)
+for (var i = 0; i <= 2; i++)
     {
     console.log(movies[i]);
 }*/
