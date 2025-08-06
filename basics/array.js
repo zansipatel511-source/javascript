27f83698d3978fc1bd4732008ba83de28e12b6fd
zansipatel511-source <zansipatel511@gmail.com>
2025-08-06 10:56:40 +0000

    array update

--- basics/array.js
+++ basics/array.js
@@ -169,11 +169,11 @@ console.log("Average is ", average);*/
 
 
 
-var marks = [10, 15, 19, 20, 21, 45, 31, 18];
+/*var marks = [10, 15, 19, 20, 21, 45, 31, 18];
 var max = -Infinity;
 for (var i = 0; i < marks.length; i++) {
     if (max < marks[i]) {
         max = marks[i];
     }
 }
-console.log(max);
+console.log(max);*/
