9e5103c4a03a87c266edfd6b3aad142c7b9fc138
zansipatel511-source <zansipatel511@gmail.com>
2025-08-07 11:20:30 +0000

    string done

--- basics/string.js
+++ basics/string.js
@@ -3,5 +3,21 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value"); //
 
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);//
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount} `);
+const gameName = new String('Zansi-Patel') 
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(3));
+/*console.log(gameName.indexOf('n'));*/
+
+//const newString = gameName.substring(0, 4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-2, 4)
+console.log(anotherString);
