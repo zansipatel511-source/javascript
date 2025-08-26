56a77e31ffa5623ca803bd13e49abdd92a3cecfb
zansipatel511-source <zansipatel511@gmail.com>
2025-08-26 10:39:46 +0000

    string done

---
+++ localstorage/string.js
@@ -0,0 +1,19 @@
+var emp=
+{
+"Employees" : [
+{
+"userId":"sqwert",
+"jobTitleName":"Developer",
+"firstName":"Sam",
+"lastName":"Pat",
+
+    },
+    {
+    "userId":"amir1337",
+    "jobTitleName":"Front-end Developer",
+    "firstName":"Amir",
+    "lastName":"Khan",        }
+],
+"count": 2
+
+}
